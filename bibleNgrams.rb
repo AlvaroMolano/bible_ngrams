1c2eea01e7fe0b6523e90ed8c31e5ff0d7772825
Snarky <thesnarky1@users.noreply.github.com>
2014-08-24 07:42:51 -0400

    Restructred corpus folder to allow for multiple translations

--- bibleNgrams.rb
+++ bibleNgrams.rb
@@ -7,50 +7,25 @@ require_relative('ngram')
 require_relative('corpus')
 require_relative('bibleCorpusFile')
 
-@all_unigrams = Hash.new(0)
-@all_bigrams = Hash.new(0)
-@all_trigrams = Hash.new(0)
+def analyze_all_books(n, minimum)
+  ngrams = Hash.new(0)
+  (1..2).each do |book|
+    analyze_book(n, book, ngrams)
+  end
+end
 
-def analyze_book(n, minimum)
+def analyze_book(n, book, results)
   corpus = Corpus.new("./corpus/books/#{n}.csv", BibleCorpusFile)
 
-  unigrams = Hash.new(0);
-  bigrams = Hash.new(0);
-  trigrams = Hash.new(0);
-  puts "Parsing unigrams"
-
   # Write out book-specific results
-  corpus.unigrams.each do |result|
-    unigrams[result.join(' ')] += 1
-    @all_unigrams[result.join(' ')] += 1
+  corpus.ngrams(n).each do |result|
+    results[book][result.join(' ')] += 1
   end
+end
+
+def write_result(results, minimum=1)
   CSV.open("./results/unigram/#{n}.csv", "w") do |csv|
-    unigrams.keep_if{|k, v| v > minimum}.sort_by{|k, v| [-v, k]}.each do |row|
-      gram = row[0]
-      count = row[1]
-      csv << [gram, count, n]
-    end
-  end
-  puts "Parsing bigrams"
-  corpus.bigrams.each do |result|
-    bigrams[result.join(' ')] += 1
-    @all_bigrams[result.join(' ')] += 1
-  end
-  CSV.open("./results/bigram/#{n}.csv", "w") do |csv|
-    bigrams.keep_if{|k, v| v > minimum}.sort_by{|k, v| [-v, k]}.each do |row|
-      gram = row[0]
-      count = row[1]
-      csv << [gram, count, n]
-    end
-  end
-  puts "Parsing trigrams"
-  corpus.trigrams.each do |result|
-    trigrams[result.join(' ')] += 1
-    @all_trigrams[result.join(' ')] += 1
-  end
-  trigrams.sort_by{|k, v| [-v, k]}
-  CSV.open("./results/trigram/#{n}.csv", "w") do |csv|
-    trigrams.keep_if{|k, v| v > minimum}.sort_by{|k, v| [-v, k]}.each do |row|
+    results[:all].keep_if{|k, v| v >= minimum}.sort_by{|k, v| [-v, k]}.each do |row|
       gram = row[0]
       count = row[1]
       csv << [gram, count, n]
