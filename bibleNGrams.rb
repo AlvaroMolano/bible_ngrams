6acf2f45efd5dfed856e4c115d215067100b16e8
Snarky <thesnarky1@users.noreply.github.com>
2014-08-24 15:51:48 -0400

    Removing a slew of now defunct files

--- bibleNGrams.rb
+++ bibleNGrams.rb
@@ -7,59 +7,68 @@ require_relative('classes/ngram')
 require_relative('classes/corpus')
 require_relative('classes/bibleCorpusFile')
 
-def analyze_all_books(n, minimum)
-  ngrams = Hash.new(0)
-  (1..2).each do |book|
-    analyze_book(n, book, ngrams)
-  end
-end
-
-def analyze_book(n, book, results)
-  corpus = Corpus.new("./corpus/books/#{n}.csv", BibleCorpusFile)
-
-  # Write out book-specific results
-  corpus.ngrams(n).each do |result|
-    results[book][result.join(' ')] += 1
-  end
-end
-
-def write_result(results, minimum=1)
-  CSV.open("./results/unigram/#{n}.csv", "w") do |csv|
-    results[:all].keep_if{|k, v| v >= minimum}.sort_by{|k, v| [-v, k]}.each do |row|
-      gram = row[0]
-      count = row[1]
-      csv << [gram, count, n]
+# This will kick off a massive run checking all n-grams
+# from 1-10 across all versions. It will be slow.
+def analyze_all_versions
+  Dir.glob('./corpus/*/') do |version|
+    version_name = /corpus\/(.+)\//.match(version)[1]
+    (1..10).each do |x|
+      write_result(analyze_version(version_name, x))
     end
   end
 end
 
-(1..66).each do |n|
-  puts "Analyzing book #{n}"
-  analyze_book(n, 0)
+# Generate base ngram statistics for a particular version 
+# (stored in ./corpus/#{version}/)
+# for example: analyze_version('kjv', 1)
+def analyze_version(version, n)
+  ngrams = Hash.new(0)
+  ngrams[:all] = Hash.new(0)
+  ngrams[:n] = n
+  ngrams[:version] = version
+  (1..66).each do |book|
+    ngrams[book] = analyze_book(version, book, n)
+    ngrams[:all].merge!(ngrams[book]){|key, oldVal, newVal| oldVal + newVal}
+  end
+  return ngrams
 end
 
-puts "Writing out aggregate results"
-CSV.open("./results/unigram/aggregate.csv", "w") do |csv|
-  @all_unigrams.keep_if{|k, v| v > 0}.sort_by{|k, v| [-v, k]}.each do |row|
-    gram = row[0]
-    count = row[1]
-    csv << [gram, count]
+# Does the work to analyze a particular book in a particular version 
+# and provide the statistics as a hash we can add to later
+def analyze_book(version, book, n)
+  corpus = Corpus.new("./corpus/#{version}/#{version}_#{book}.csv", BibleCorpusFile)
+  results = Hash.new(0)
+  # Keep book-specific results
+  corpus.ngrams(n).each do |result|
+    results[result.join(' ')] += 1
   end
+  return results
 end
-CSV.open("./results/bigram/aggregate.csv", "w") do |csv|
-  @all_bigrams.keep_if{|k, v| v > 0}.sort_by{|k, v| [-v, k]}.each do |row|
-    gram = row[0]
-    count = row[1]
-    csv << [gram, count]
-  end
-end
-CSV.open("./results/trigram/aggregate.csv", "w") do |csv|
-  @all_trigrams.keep_if{|k, v| v > 0}.sort_by{|k, v| [-v, k]}.each do |row|
-    gram = row[0]
-    count = row[1]
-    csv << [gram, count]
+
+def write_result(results, minimum=1)
+  n = results[:n]
+  version = results[:version]
+  results.keys.each do |key, val|
+  if key.is_a? Integer    
+    CSV.open("./results/#{n}-gram/#{version}/#{version}_#{book}.csv", "w") do |csv|
+      csv << ["N-gram", "Count", "Book"]
+      results[key].keep_if{|k, v| v >= minimum}.sort_by{|k, v| [-v, k]}.each do |row|
+        gram = row[0]
+        count = row[1]
+        csv << [gram, count, book]
+      end
+    end
+  else if key == :all
+    CSV.open("./results/#{n}-gram/#{version}.csv", "w") do |csv|
+      csv << ["N-gram", "Count", "Version"]
+      results[:all].keep_if{|k, v| v >= minimum}.sort_by{|k, v| [-v, k]}.each do |row|
+        gram = row[0]
+        count = row[1]
+        csv << [gram, count, version]
+      end
+    end
   end
 end
 
-#puts results.keep_if{|k, v| v > 0}.sort_by{|k, v| v}.inspect
-#puts results.keep_if{|k, v| k.include?("Amen")}.sort_by{|k, v| v}.inspect
+#test = analyze_version("kjv", 2)
+#uts test[:all].sort_by{|k, v| [-v, k]}.inspect
